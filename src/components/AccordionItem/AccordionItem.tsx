0a89a6aedd8f858804707c306a5a719946e0b0ed
Alexander <aleksievisa@gmail.com>
2023-05-20 15:46:41 +0300

    feat: add accordion

--- src/components/AccordionItem/AccordionItem.tsx
+++ src/components/AccordionItem/AccordionItem.tsx
@@ -3,14 +3,29 @@ import styles from './AccordionItem.module.scss'
 interface IAccordionItem {
   name: string
   count: string
+  active: boolean
+  setActive: (val: string) => void
+  img: string
+  date: string
 }
 
-export const AccordionItem = ({ name, count }: IAccordionItem) => {
+export const AccordionItem = ({ name, img, date, count, active, setActive }: IAccordionItem) => {
   return (
     <div className={styles.wrapper}>
-      <h3 className={styles.name}>{name}</h3>
-      <span className={styles.count}>{count}</span>
-      <hr className={styles.line} />
+      <div className={styles.item} onClick={() => setActive(name)}>
+        <h3 className={styles.name}>{name}</h3>
+        <span className={styles.count}>{count}</span>
+        <hr className={styles.line} />
+      </div>
+      <div className={`${styles.card} ${active && styles.active}`}>
+        <img src={img} />
+        <aside className={styles.aside}>
+          <h4 className={styles.name}>{name}</h4>
+          <span className={styles.date}>{date}</span>
+          <button className={styles.btn}>More information</button>
+        </aside>
+        <hr className={styles.line} />
+      </div>
     </div>
   )
 }
--- src/view/EventsPage/EventPage.tsx
+++ src/view/EventsPage/EventPage.tsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import { AccordionItem } from '../../components/AccordionItem/AccordionItem';
 import styles from './EventPage.module.scss';
+import ImageAcc from '../../assets/img/one-event.jpg'
 
 const dataAccordion = {
-  one: { name: 'Hawaiian party', count: '01' },
-  two: { name: 'Мafia party', count: '02' },
-  three: { name: 'Party', count: '03' },
-  four: { name: 'Party on the beach', count: '04' },
-  five: { name: 'Home Security', count: '05' },
-  six: { name: 'Network Design & Implementation', count: '06' },
-  seven: { name: 'System Design & Engineering', count: '07' },
-  eight: { name: 'Client Care Plans', count: '08' },
+  one: { name: 'Hawaiian party', count: '01', data: '13.02.2023' },
+  two: { name: 'Мafia party', count: '02', data: '13.02.2023' },
+  three: { name: 'Party', count: '03', data: '13.02.2023' },
+  four: { name: 'Party on the beach', count: '04', data: '13.02.2023' },
+  five: { name: 'Home Security', count: '05', data: '13.02.2023' },
+  six: { name: 'Network Design & Implementation', count: '06', data: '13.02.2023' },
+  seven: { name: 'System Design & Engineering', count: '07', data: '13.02.2023' },
+  eight: { name: 'Client Care Plans', count: '08', data: '13.02.2023' },
 }
 
 export const EventPage = () => {
+  const [activeCard, setActiveCard] = useState<string>(dataAccordion.one.name)
+
+  const handlerActive = (value: string) => {
+    setActiveCard(value)
+  }
+
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.title}>ALL EVENTS</h2>
@@ -23,6 +31,10 @@ export const EventPage = () => {
             key={item.name} 
             count={item.count}
             name={item.name}
+            active={activeCard === item.name}
+            date={item.data}
+            img={ImageAcc}
+            setActive={handlerActive}
           />
         ))}
       </div>
